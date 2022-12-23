26ba821ae8caff428182b9d4b852496bf4a5614a
kun <kun>
2022-12-23 15:23:41 -0500

    format changes

--- app/src/main/java/com/example/androidtutorial/tutorial/TestFile.kt
+++ app/src/main/java/com/example/androidtutorial/tutorial/TestFile.kt
@@ -1,27 +1,32 @@
 package com.example.androidtutorial.tutorial
+
 // higher order function and lamda
 // overall android architecture
 // textview, button
-fun main(){
-    add1(1,3)
-    sub(3,5)
-    val a : Boolean = true
-    val somefuntion: (String) -> String = {it.reversed() }
+// github with source tree
+fun main() {
+    add1(1, 3)
+    sub(3, 5)
+    val a: Boolean = true
+    val somefuntion: (String) -> String = { it.reversed() }
 }
-fun add1(a: Int, b:Int): Int{
+
+fun add1(a: Int, b: Int): Int {
     return a + b
 }
-fun sub(a: Int, b: Int): Int{
+
+fun sub(a: Int, b: Int): Int {
     return a - b
 }
+
 fun myPrint() {
     println("SomePrint")
 }
-fun higherOrderFunction(a: Int, b:Int, action: (Int, Int)->Int ){
+
+fun higherOrderFunction(a: Int, b: Int, action: (Int, Int) -> Int) {
     println(a)
     println(b)
-    println(action(a,b))
+    println(action(a, b))
 }
 // myapp.apk
 // .dex
-
