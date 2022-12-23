da822936b028afda8949e03c42f31f38d5cdbb7e
kun <kun>
2022-12-23 15:46:20 -0500

    some changes

--- app/src/main/java/com/example/androidtutorial/MainActivity.kt
+++ app/src/main/java/com/example/androidtutorial/MainActivity.kt
@@ -7,7 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val c = 2
-        val b = "someString"
     }
 }
