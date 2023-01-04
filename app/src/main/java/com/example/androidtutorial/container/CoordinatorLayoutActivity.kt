a0e661f3c3de145e39a62dc111010ed0afca3788
kun <kun>
2023-01-04 15:20:53 -0500

    group 1

--- app/src/main/java/com/example/androidtutorial/container/CoordinatorLayoutActivity.kt
+++ app/src/main/java/com/example/androidtutorial/container/CoordinatorLayoutActivity.kt
@@ -7,15 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
 
 class CoordinatorLayoutActivity : AppCompatActivity() {
-    lateinit var btnLayoutBased : Button
-    lateinit var btnScrollBased : Button
+    lateinit var btnLayoutBased: Button
+    lateinit var btnScrollBased: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coordinator_layout)
         btnLayoutBased = findViewById(R.id.btnLayoutBased)
         btnScrollBased = findViewById(R.id.btnScrollBased)
 
-        btnLayoutBased.setOnClickListener{
+        btnLayoutBased.setOnClickListener {
             val intent = Intent(this, LayoutBasedActivity::class.java)
             startActivity(intent)
         }
