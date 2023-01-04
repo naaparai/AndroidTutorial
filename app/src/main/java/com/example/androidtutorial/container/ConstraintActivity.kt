affd4257f778abb11e1e607181720bfb21bf3c35
minleg <minleg@minleg.com>
2023-01-04 14:24:33 -0600

    djfadls

--- app/src/main/java/com/example/androidtutorial/container/ConstraintActivity.kt
+++ app/src/main/java/com/example/androidtutorial/container/ConstraintActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtutorial.R
 
 class ConstraintActivity : AppCompatActivity() {
+    // comment here
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint)
